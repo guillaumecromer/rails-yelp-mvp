42f3bcc25ef6a894ce0985740cb3101511feea76
guillaume cromer <guillaumecromer@gmail.com>
2019-08-14 12:29:17 +0200

    migration for restaurants_id

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,13 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+private
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_094007) do
+ActiveRecord::Schema.define(version: 2019_08_14_102702) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_08_14_094007) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
